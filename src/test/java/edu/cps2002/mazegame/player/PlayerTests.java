2cabd22cd09991f2ad94e98f1e64dee21db6859f
francescac5 <francesca.chircop.18@um.edu.mt>
2020-04-14 14:50:15 +0200

    Implemented the tests for the setter and getter of position found in the class Player

---
+++ src/test/java/edu/cps2002/mazegame/player/PlayerTests.java
@@ -0,0 +1,32 @@
+package edu.cps2002.mazegame.player;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class PlayerTests {
+     public Player p;
+    @Before
+    public void setup() {
+        p = new Player();
+    }
+    @After
+    public void tearDown() {
+        p = null;
+    }
+
+    //testing SetPosition
+    @Test
+    public void testSetPosition(){
+        p.setPosition(10);
+        assertEquals(10, p.getPosition());
+    }
+
+    //testing GetPosition
+    @Test
+    public void testGetPosition(){
+        assertEquals(3, p.getPosition());
+    }
+
+}
