be2fed6123034953cd12c2ffb3e80275622e13c7
francescac5 <francesca.chircop.18@um.edu.mt>
2020-04-14 16:18:25 +0200

    Implemented the setPlayer method

--- src/main/java/edu/cps2002/mazegame/player/Player.java
+++ src/main/java/edu/cps2002/mazegame/player/Player.java
@@ -2,11 +2,12 @@ package edu.cps2002.mazegame.player;
 
 public class Player {
     private Position position;
-    public void setPosition(Position pn) {
-    }
 
     public Position getPosition() {
-        //this.Position = position;
         return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
     }
 }
