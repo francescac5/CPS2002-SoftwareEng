5851c510e0ae5c13dfae20b10bb3deabc0e1499e
francescac5 <francesca.chircop.18@um.edu.mt>
2020-04-16 10:08:06 +0200

    Worked on the chooseDirection method

--- src/main/java/edu/cps2002/mazegame/game/Game.java
+++ src/main/java/edu/cps2002/mazegame/game/Game.java
@@ -3,7 +3,19 @@ package edu.cps2002.mazegame.game;
 import edu.cps2002.mazegame.player.Player;
 
 public class Game {
-//    public static Player.DIRECTION chooseDirection(String d) {
-//        return Player.DIRECTION.UP;
-//    }
+    public static Player.DIRECTION chooseDirection(String input) {
+        while (true) {
+            if (input.equalsIgnoreCase("U")) {
+                return Player.DIRECTION.UP;
+            } else if (input.equalsIgnoreCase("D")) {
+                return Player.DIRECTION.DOWN;
+            } else if (input.equalsIgnoreCase("L")) {
+                return Player.DIRECTION.LEFT;
+            } else if (input.equalsIgnoreCase("R")) {
+                return Player.DIRECTION.RIGHT;
+            } else {
+                System.out.println("ERROR: Enter a valid direction");
+            }
+        }
+    }
 }
--- src/test/java/edu/cps2002/mazegame/game/TestGame.java
+++ src/test/java/edu/cps2002/mazegame/game/TestGame.java
@@ -14,43 +14,43 @@ public class TestGame {
     public Player p1;
     Position pos = new Position(4, 5);
 
-//    @Before
-//    public void setup() {
-//        Position pos = new Position(4, 5);
-//        p1 = new Player(4, 5);
-//    }
-//
-//    @After
-//    public void tearDown() {
-//        p1 = null;
-//        pos = null;
-//    }
+    @Before
+    public void setup() {
+        Position pos = new Position(4, 5);
+        p1 = new Player(4, 5);
+    }
 
-//    //testing chooseDirection down
-//    @Test
-//    public void testchooseDirectionD() {
-//        Assert.assertEquals(Player.DIRECTION.DOWN, Game.chooseDirection("D"));
-//
-//    }
-//
-//    //testing chooseDirection up
-//    @Test
-//    public void testchooseDirectionU() {
-//        assertEquals(Player.DIRECTION.UP, Game.chooseDirection("U"));
-//
-//    }
-//
-//    //testing chooseDirection left
-//    @Test
-//    public void testchooseDirectionL() {
-//        assertEquals(Player.DIRECTION.LEFT, Game.chooseDirection("L"));
-//
-//    }
-//
-//    //testing chooseDirection right
-//    @Test
-//    public void testchooseDirectionR() {
-//        assertEquals(Player.DIRECTION.RIGHT, Game.chooseDirection("R"));
-//
-//    }
+    @After
+    public void tearDown() {
+        p1 = null;
+        pos = null;
+    }
+
+    //testing chooseDirection down
+    @Test
+    public void testchooseDirectionD() {
+        Assert.assertEquals(Player.DIRECTION.DOWN, Game.chooseDirection("D"));
+
+    }
+
+    //testing chooseDirection up
+    @Test
+    public void testchooseDirectionU() {
+        assertEquals(Player.DIRECTION.UP, Game.chooseDirection("U"));
+
+    }
+
+    //testing chooseDirection left
+    @Test
+    public void testchooseDirectionL() {
+        assertEquals(Player.DIRECTION.LEFT, Game.chooseDirection("L"));
+
+    }
+
+    //testing chooseDirection right
+    @Test
+    public void testchooseDirectionR() {
+        assertEquals(Player.DIRECTION.RIGHT, Game.chooseDirection("R"));
+
+    }
 }
