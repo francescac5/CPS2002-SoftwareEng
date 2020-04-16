6d4840a442ed1817a7f06323d8507e119ef3a1f2
francescac5 <francesca.chircop.18@um.edu.mt>
2020-04-16 11:25:11 +0200

    Implemented the method getNumPlayers to ask the user for the number of players in the game.

--- src/main/java/edu/cps2002/mazegame/game/Game.java
+++ src/main/java/edu/cps2002/mazegame/game/Game.java
@@ -2,6 +2,8 @@ package edu.cps2002.mazegame.game;
 
 import edu.cps2002.mazegame.player.Player;
 
+import java.util.Scanner;
+
 public class Game {
     static int minPlayers = 2;
     static int maxPlayers = 8;
@@ -28,5 +30,30 @@ public class Game {
         }else{
             return false;
         }
+    }
+
+    //method to get the number of players from the user
+    public static int getNumPlayers(){
+        int numPlayers = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("Please choose the number of players: \n");
+            try {
+                numPlayers = sc.nextInt();
+            } catch (Exception e) {
+                sc.next();
+            } finally {
+                if(validityofPlayers(numPlayers)){
+                    return numPlayers;
+
+                }else{
+                    System.out.println("===The number of players should be between 2 and 8===\n");
+                }
+            }
+        } while(true);
+    }
+
+    public static void main(String[] args) {
+        getNumPlayers();
     }
 }
