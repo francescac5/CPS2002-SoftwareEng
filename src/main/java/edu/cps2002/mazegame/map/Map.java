5f8556e2f54b51e39803e7aea379d8e1b4bdc43b
francescac5 <francesca.chircop.18@um.edu.mt>
2020-04-17 17:24:33 +0200

    Commiting changes in order to merge

--- src/main/java/edu/cps2002/mazegame/map/Map.java
+++ src/main/java/edu/cps2002/mazegame/map/Map.java
@@ -1,17 +1,219 @@
 package edu.cps2002.mazegame.map;
 
+import edu.cps2002.utils.MapUtils;
+import javafx.util.Pair;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Map {
-    private int size;
+
+    public enum Tiles{
+        GRASS,
+        WATER,
+        TREASURE, //1 tile
+        GREY,
+        GRASS_PLAYER,
+        GRASS_INIT
+    }
+
+    private MapUtils util = new MapUtils();
+
+    private int size = -1;
+    private static int mapCount;
+    private Tiles[][] mapTiles;
+    private boolean tilesGenerated = false;
+
+    private ArrayList<Pair<Integer,Integer>> grassTiles = new ArrayList<>();
+    private ArrayList<Pair<Integer,Integer>> waterTiles = new ArrayList<>();
+    private Pair<Integer, Integer> treasureTile;
+
+    private ArrayList<Tiles[][]> playerMaps = new ArrayList<>();
+
+    public void initMapCount(){
+        mapCount = 0;
+    }
+
+    //only for testing purposes
+    protected int getMapCount() {
+        return mapCount;
+    }
 
     public boolean setMapSize(int size) {
         //if inputted size is less than minimum of 5
-        if(size < 5){
+        if(size < 5 || size > 50){
             return false;
         }
         else{
             this.size = size;
             return true;
         }
+    }
 
+    public int getMapSize() {
+        return size;
+    }
+
+    public void generate(){
+
+        //single set of tiles
+        if(!tilesGenerated){
+            generateTileTypes();
+            tilesGenerated = true;
+        }
+
+        mapCount++;
+        Tiles [][] playerMap = generateInitMap();
+        playerMaps.add(playerMap);
+
+        util.generateMapHTML(mapCount, playerMap);
+    }
+
+    private Tiles[][] generateInitMap() {
+        Tiles[][] initMap = new Tiles[size][size];
+
+        Collections.shuffle(grassTiles);
+        Pair<Integer, Integer> initTile = grassTiles.get(0);
+
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                if(initTile.getKey() == x && initTile.getValue() == y){
+                    initMap[x][y] = Tiles.GRASS_INIT;
+                }else {
+                    initMap[x][y] = Tiles.GREY;
+                }
+            }
+        }
+        return initMap;
+    }
+
+    private void deleteMaps() {
+        util.deleteHTMLFiles();
+    }
+
+    protected void generateTileTypes() {
+        Pair<Integer, Integer> temp;
+        int amountTiles = size * size;
+        int tileCount = 1;
+
+        //generate grass tiles
+        ArrayList<Integer> grass = generateGrassTiles(amountTiles);
+
+        //generate treasure tile
+        int treasure = generateTreasureTile(amountTiles, grass);
+
+        //generate water tiles
+        ArrayList<Integer> water = generateWaterTiles(amountTiles, treasure, grass);
+
+        mapTiles = new Tiles[size][size];
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                if (tileCount == treasure) {
+                    mapTiles[x][y] = Tiles.TREASURE;
+
+                    treasureTile = new Pair<>(x, y);
+
+                } else if (grass.contains(tileCount)) {
+                    mapTiles[x][y] = Tiles.GRASS;
+
+                    temp = new Pair<>(x, y);
+                    grassTiles.add(temp);
+
+                } else if (water.contains(tileCount)) {
+                    mapTiles[x][y] = Tiles.WATER;
+
+                    temp = new Pair<>(x, y);
+                    waterTiles.add(temp);
+                }
+                tileCount++;
+            }
+        }
+    }
+
+    private ArrayList<Integer> generateGrassTiles(int amountTiles) {
+        ArrayList<Integer> grassTiles = new ArrayList<Integer>();
+
+        Random r = new Random();
+        int randNum;
+        int amountGrass = (int)Math.ceil(0.85*amountTiles);
+
+        randNum = r.nextInt(amountGrass) + 1;
+        grassTiles.add(randNum);
+
+        for (int i = 1; i < amountGrass; i++) {
+            do{
+                randNum = r.nextInt(amountTiles) + 1;
+            }while(grassTiles.contains(randNum));
+            grassTiles.add(randNum);
+        }
+        return grassTiles;
+    }
+
+    private int generateTreasureTile(int amountTiles, ArrayList<Integer> grassTiles) {
+        //set treasure tile to a tile which is not a grass tile
+        Random r = new Random();
+        int treasure;
+        do{
+            treasure = r.nextInt(amountTiles) + 1;
+        }while(grassTiles.contains(treasure));
+        return treasure;
+    }
+
+    private ArrayList<Integer> generateWaterTiles(int amountTiles, int treasure, ArrayList<Integer> grassTiles) {
+        ArrayList<Integer> waterTiles = new ArrayList<Integer>();
+
+        for (int i = 1; i < amountTiles+1; i++) {
+            if(!grassTiles.contains(i) && i != treasure){
+                waterTiles.add(i);
+            }
+        }
+        return waterTiles;
+    }
+
+    protected Tiles[][] getTiles() {
+        return this.mapTiles;
+    }
+
+    protected ArrayList<Pair<Integer, Integer>> getGrassTiles() {
+        return grassTiles;
+    }
+
+
+    protected ArrayList<Pair<Integer, Integer>> getWaterTiles() {
+        return waterTiles;
+    }
+
+    protected Pair<Integer, Integer> getTreasureTile() {
+        return treasureTile;
+    }
+
+    public char getTileType(int x, int y) {
+        if(x >= size || y >= size){
+            return 'E';
+        }
+
+        char type;
+
+        switch(mapTiles[x][y]){
+            case TREASURE:
+                type = 'T';
+                break;
+            case GRASS:
+                type = 'G';
+                break;
+            case WATER:
+                type = 'W';
+                break;
+            default:
+                type = 'E';
+                break;
+        }
+        return type;
     }
 }
+
--- src/main/java/edu/cps2002/utils/MapUtils.java
+++ src/main/java/edu/cps2002/utils/MapUtils.java
@@ -1,4 +1,97 @@
 package edu.cps2002.utils;
 
+import edu.cps2002.mazegame.map.Map;
+import edu.cps2002.mazegame.map.PlayerTile;
+import edu.cps2002.mazegame.map.Tile;
+
+import java.io.*;
+
 public class MapUtils {
+
+    public void writeToFile(String path, String content){
+        try {
+            File file = new File(path);
+            if (file.createNewFile()) {
+                System.out.println("File created: "+ file.getName());
+            } else {
+                System.out.println("File "+ file.getName()+" already exists.");
+            }
+
+            FileWriter fWriter;
+            BufferedWriter writer;
+            try {
+                fWriter = new FileWriter(file);
+                writer = new BufferedWriter(fWriter);
+
+                writer.write(content);
+
+                writer.newLine();
+                writer.close();
+            }
+            catch (Exception e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public void generateMapHTML(int mapCount, Map.Tiles[][] playerMap) {
+        String path = "src\\main\\java\\edu\\cps2002\\mazegame\\gameMaps\\map_player_"+mapCount+".html";
+
+        StringBuilder mapHTML = new StringBuilder();
+
+        mapHTML.append("<html>\n");
+        mapHTML.append("<body>\n");
+        mapHTML.append("<table border ='1'>\n");
+        mapHTML.append("<thead>\n");
+        mapHTML.append("<tr>\n");
+        mapHTML.append("<th colspan=\"");
+            mapHTML.append(playerMap.length);
+            mapHTML.append("\">Player ");
+            mapHTML.append(mapCount);
+            mapHTML.append("</th>\n");
+        mapHTML.append("</tr>\n");
+        mapHTML.append("<tbody>\n");
+
+        //inputting grid according to map size
+        for (int y = 0; y < playerMap.length; y++) {
+            mapHTML.append("<tr>");
+            for (int x = 0; x < playerMap.length; x++) {
+                if(playerMap[x][y] == Map.Tiles.GRASS){
+                    mapHTML.append(new Tile().getTileHTML("green"));
+                }
+                else if(playerMap[x][y] == Map.Tiles.GRASS_PLAYER || playerMap[x][y] == Map.Tiles.GRASS_INIT) {
+                    mapHTML.append(new PlayerTile().getTileHTML("green"));
+                }
+                else if(playerMap[x][y] == Map.Tiles.WATER) {
+                    mapHTML.append(new Tile().getTileHTML("blue"));
+                }
+                else if(playerMap[x][y] == Map.Tiles.TREASURE) {
+                    mapHTML.append(new PlayerTile().getTileHTML("yellow"));
+                }
+                else if(playerMap[x][y] == Map.Tiles.GREY) {
+                    mapHTML.append(new Tile().getTileHTML("grey"));
+                }
+            }
+            mapHTML.append("</tr>");
+        }
+
+        mapHTML.append("</tbody>\n");
+        mapHTML.append("</thead>\n");
+        mapHTML.append("</table>\n");
+
+        writeToFile(path, mapHTML.toString());
+    }
+
+    public void deleteHTMLFiles(){
+        File mapFolder = new File("src\\main\\java\\edu\\cps2002\\mazegame\\gameMaps")   ;
+
+        File[] maps = mapFolder.listFiles();
+        for(File map: maps){
+            map.delete();
+        }
+    }
 }
