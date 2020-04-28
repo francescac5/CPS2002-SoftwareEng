37f2dd7fdf5bb7e7b7edc9a4451c7b6a8922390d
francescac5 <francesca.chircop.18@um.edu.mt>
2020-04-28 18:41:58 +0200

    Added more comments to make the code more readable

--- src/main/java/edu/cps2002/mazegame/player/Player.java
+++ src/main/java/edu/cps2002/mazegame/player/Player.java
@@ -5,24 +5,28 @@ import edu.cps2002.mazegame.map.Map;
 public class Player {
     private Position position;
 
-
+   //getter for the position
     public Position getPosition() {
         return position;
     }
 
+    //constructor for the player
     public Player(int x, int y) {
         Position p = new Position(x, y);
         setPosition(p);
     }
 
+    //enum for the direction
     public enum DIRECTION {
         UP, DOWN, RIGHT, LEFT
     }
 
+     //setter for the position
     public void setPosition(Position position) {
         this.position = position;
     }
 
+    //method move to change the x and y coordinates accordingly 
     public boolean move(DIRECTION Direction) {
         int newX = position.getX();
         int newY = position.getY();
@@ -41,6 +45,7 @@ public class Player {
                 break;
         }
 
+    //checking the x and y axis in order to ensure that the user does not try to move outside the map
         if (newX < 0 || newX >= Map.getMapSize()
                 || newY < 0 || newY >= Map.getMapSize()){
             return false;
