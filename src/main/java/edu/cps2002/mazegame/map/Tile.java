021731210a2dc225171220099b0bb8766ffd89ad
Esther Spiteri <esther.spiteri.18@um.edu.mt>
2020-05-01 11:44:45 +0200

    Implemented comments for Player Tile and Tile and updated those for Map

--- src/main/java/edu/cps2002/mazegame/map/Tile.java
+++ src/main/java/edu/cps2002/mazegame/map/Tile.java
@@ -2,6 +2,7 @@ package edu.cps2002.mazegame.map;
 
 public class Tile {
 
+    //Returns HTML for tile with given colour
     public String getTileHTML(String tileColour){
         return "<td height=\"50\" width=\"50\" style=\"background-color:"+tileColour+";\"></td>\n";
     }
