4cafc708ff559335ad0d74dcb9b3a189b552bf7a
Esther Spiteri <esther.spiteri.18@um.edu.mt>
2020-05-15 23:00:45 +0200

    -Implemented createMap in MapFactory

--- src/main/java/edu/cps2002/mazegame/map/MapFactory.java
+++ src/main/java/edu/cps2002/mazegame/map/MapFactory.java
@@ -1,7 +1,13 @@
 package edu.cps2002.mazegame.map;
 
 public class MapFactory {
-    public Map createMap(String safe) {
-        return new Map();
+    public Map createMap(String type) {
+        if (type.equals("safe")){
+            return new SafeMap();
+        }
+        else if (type.equals("hazardous")){
+            return new HazardousMap();
+        }
+        return null;
     }
 }
