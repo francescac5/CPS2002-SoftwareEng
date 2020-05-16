7c9551f23688eba116376a41c0c6289e6894f875
francescac5 <francesca.chircop.18@um.edu.mt>
2020-05-16 09:57:18 +0200

    Added the tests with the skeleton code

--- src/test/java/edu/cps2002/mazegame/map/TestMapSingleton.java
+++ src/test/java/edu/cps2002/mazegame/map/TestMapSingleton.java
@@ -3,15 +3,38 @@ package edu.cps2002.mazegame.map;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class TestMapSingleton {
 
+    @Test
+    public void getInstanceTest1(){
+
+        //Instance 1
+        MapSingleton instance1 = MapSingleton.getInstance();
+
+        //Instance 2
+        MapSingleton instance2 = MapSingleton.getInstance();
+
+        //checking that the hashcode is equal
+        assertEquals(instance1.hashCode(),instance2.hashCode());
+    }
 
     @Test
-    public void getInstanceTest(){
-        Map singleton = MapSingleton.getInstance();
+    public void getInstanceTest2(){
+        //  Map singleton = MapSingleton.getInstance();
 
-     //   assertThat(StoreMap.getInstance(), singleton);
+        //Instance 1
+        MapSingleton instance1 = MapSingleton.getInstance();
+
+        //Instance 2
+        MapSingleton instance2 = MapSingleton.getInstance();
+
+        //Instance 3
+        MapSingleton instance3 = MapSingleton.getInstance();
+
+        //checking that the hashcode is equal
+        assertEquals(instance1.hashCode(),instance3.hashCode());
     }
 
 }
